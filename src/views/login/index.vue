d7a415ff1bb104581ba7bf1777f8bfab88f85ff2
love-cong <2404662491@qq.com>
2019-12-11 11:08:13 +0800

    登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,21 +5,25 @@
          <div class="title">
             <img src="../../assets/logo_index.png" alt="">
          </div>
-         <el-form style="margin-top:20px">
-             <el-form-item>
-                 <el-input></el-input>
+         <!-- 数据校验要先给el-form绑定nodel, 绑定rules规则 -->
+         <el-form ref="myForm" :model="loginForm" :rules="loginRules" style="margin-top:20px">
+             <el-form-item prop="mobile">
+                 <!-- 手机号 绑定v-model -->
+                 <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
              </el-form-item>
-             <el-form-item>
+             <el-form-item prop="code">
                  <!-- 验证码 -->
-                 <el-input style="width:65%"></el-input>
+                 <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:65%"></el-input>
                  <!-- 发送验证码 -->
                  <el-button style="float:right">发送验证码</el-button>
              </el-form-item>
-             <el-form-item>
-                 <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+             <el-form-item prop="agree">
+               <!-- 同意选项 -->
+                 <el-checkbox v-model="loginForm.agree">我已阅读并同意用户协议和隐私条款</el-checkbox>
              </el-form-item>
              <el-form-item>
-                 <el-button style="width:100%" type="primary">登录</el-button>
+               <!-- 登录按钮 -->
+                 <el-button @click="login" type="primary" style="width:100%">登录</el-button>
              </el-form-item>
          </el-form>
      </el-card>
@@ -29,7 +33,54 @@
 
 <script>
 export default {
-
+//    定义一个对象
+  data () {
+    let validator = function (rule, value, callBack) {
+      // rule当前规则
+      // value当前表单项的值
+      // callBack回调函数
+      // ------------------------正常写法
+      // if (value) {
+      //   // 正确 勾选了协议
+      //   callBack() // 一切OK请继续
+      // } else {
+      //   // 不对 没勾选协议
+      //   callBack(new Error('已经阅读并同意用户协议'))
+      // }
+      // -------------------------------简单写法/炫技模式
+      value ? callBack() : callBack(new Error('已经阅读并同意用户协议'))
+    }
+    return {
+      // 表单数据是一个对象
+      // 定义一个对象
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        agree: false // 是否同意协议
+      },
+      loginRules: {
+        // 决定着校验规则 key(字段名)：value(对象数组) =>一个对象就是一校验规则
+        // required 为true 就表示该字段必填 如果不填 就会提示消息
+        mobile: [{ required: true, message: '手机号不能为空' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }],
+        code: [{ required: true, message: '验证码不能为空' },
+          { pattern: /^\d{6}$/, message: '验证码为6位数字' }],
+        agree: [{ validator }]
+      } // 登录规则集合对象
+      // validator 为自定义形式校验
+    }
+  },
+  methods: {
+    login () {
+      // 校验整个表单的规则
+      // validate 是一个方法=>方法传入的函数 两个函数 第一个是是否校验成功/第二个未校验成功字段
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          console.log('校验成功')
+        }
+      })
+    }
+  }
 }
 </script>
 
